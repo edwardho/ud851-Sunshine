66ef9b64bc4aa5519d68691f08caeba8a1e34cbc
edwardho <ekinbenho@gmail.com>
2018-12-31 18:18:37 -0500

    Completed S01.02-Exercise

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,20 +17,42 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a field to store the weather display TextView
+    // COMPLETED (1) Create a field to store the weather display TextView
+    TextView weatherDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
 
-        // TODO (2) Use findViewById to get a reference to the weather display TextView
+        // COMPLETED (2) Use findViewById to get a reference to the weather display TextView
+        weatherDisplay = (TextView) findViewById(R.id.tv_weather_data);
 
-        // TODO (3) Create an array of Strings that contain fake weather data
+        // COMPLETED (3) Create an array of Strings that contain fake weather data
+        String[] weatherData = {
+                "Today, Dec 31 - Clear - 32°F / 28°F",
+                "Tomorrow - Cloudy - 43°F / 34°F",
+                "Wednesday, Jan 2 - Clear - 43°F / 34°F",
+                "Thursday, Jan 3 - Clear - 32°F / 28°F",
+                "Friday, Jan 4 - Cloudy - 43°F / 34°F",
+                "Saturday, Jan 5 - Clear - 32°F / 28°F",
+                "Sunday, Jan 6 - Cloudy - 43°F / 34°F",
+                "Monday, Jan 7 - Clear - 32°F / 28°F",
+                "Tuesday, Jan 8 - Clear - 32°F / 28°F",
+                "Wednesday, Jan 9 - Cloudy - 43°F / 34°F",
+                "Thursday, Jan 10 - Clear - 32°F / 28°F",
+                "Friday, Jan 11 - Cloudy - 43°F / 34°F",
+                "Saturday, Jan 12 - Clear - 32°F / 28°F",
+                "Sunday, Jan 13 - Cloudy - 43°F / 34°F",
+        };
 
-        // TODO (4) Append each String from the fake weather data array to the TextView
+        // COMPLETED (4) Append each String from the fake weather data array to the TextView
+        for (String weatherDatum:weatherData) {
+            weatherDisplay.append(weatherDatum + "\n\n\n");
+        }
     }
 }
