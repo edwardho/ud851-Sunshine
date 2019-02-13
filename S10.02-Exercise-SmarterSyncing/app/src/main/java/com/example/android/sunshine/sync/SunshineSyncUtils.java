bead162dec111345bf6710bb299b9fadf97f9e38
edwardho <ekinbenho@gmail.com>
2019-02-13 16:05:36 -0500

    Completed S10.02-Exercise

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -17,18 +17,65 @@ package com.example.android.sunshine.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+//  COMPLETE (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    //  COMPLETE (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(@NonNull final Context context) {
+        //  COMPLETE (3) Only execute this method body if sInitialized is false
+        if (sInitialized) return;
+
+        //  COMPLETE (5) Check to see if our weather ContentProvider is empty
+        new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                // We have to check the ContentProvider to see if there is data
+                // in our background AsyncTask
+
+                // We only need to use the projection columns as we do not need
+                // to actually display any data
+
+                Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+
+                String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                String selectionStatement = WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards();
+                String[] args = null;
+                String sortOrder = null;
+
+                Cursor cursor = context.getContentResolver().query(
+                        forecastQueryUri,
+                        projectionColumns,
+                        selectionStatement,
+                        args,
+                        sortOrder);
+
+                //  COMPLETE (6) If it is empty or we have a null Cursor, sync the weather now!
+                // If cursor is null or empty, there is no data populated and we need to sync
+                if (cursor == null || cursor.getCount() == 0) {
+                    startImmediateSync(context);
+                }
+
+                // Close cursor to avoid memory leaks when done
+                cursor.close();
+
+                return null;
+            }
+        }.execute();
+
+        //  COMPLETE (4) If the method body is executed, set sInitialized to true
+        sInitialized = true;
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
